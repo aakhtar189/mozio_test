001c304e1e6636512581866ada698ab57e1a58e8
Asif <asif@technoarchsoftwares.com>
2017-07-11 02:20:43 +0530

    chnge in wsgi

--- mozio_test/wsgi.py
+++ mozio_test/wsgi.py
@@ -13,4 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mozio_test.settings")
 
-application = get_wsgi_application()
+def application(environ, start_response):
+    if environ['mod_wsgi.process_group'] != '': 
+        import signal
+        os.kill(os.getpid(), signal.SIGINT)
+    return ["killed"]
+
+# application = get_wsgi_application()
